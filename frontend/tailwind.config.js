1a434f8879f0d4c7c0b1acf8c8c7eb6e20784451
aigle-levant <aiglelevant@gmail.com>
2025-06-17 12:51:25 +0530

    Added file structure and components

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}",
+        "./src/**/*.component.{html,ts}",
+        "./src/app/**/*.{html,ts}",
+        "./src/components/**/*.{html,ts}",
+        "./src/pages/**/*.{html,ts}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
